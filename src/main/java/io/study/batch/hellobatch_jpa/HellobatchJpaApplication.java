5d1be907d2c427e1d85a3d54f79fa77b08647510
gosgjung <playmymind@daum.net>
2021-06-21 22:54:15 +0900

    접속 프로필 추가

--- src/main/java/io/study/batch/hellobatch_jpa/HellobatchJpaApplication.java
+++ src/main/java/io/study/batch/hellobatch_jpa/HellobatchJpaApplication.java
@@ -1,8 +1,10 @@
 package io.study.batch.hellobatch_jpa;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class HellobatchJpaApplication {
 
