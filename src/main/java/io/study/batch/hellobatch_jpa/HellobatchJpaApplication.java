671b78126a608167b5eb7663655210e5a5fb4a93
gosgjung <playmymind@daum.net>
2021-06-21 18:11:33 +0900

    spring boot 프로젝트 파일 초기 커밋

---
+++ src/main/java/io/study/batch/hellobatch_jpa/HellobatchJpaApplication.java
@@ -0,0 +1,13 @@
+package io.study.batch.hellobatch_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellobatchJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellobatchJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/study/batch/hellobatch_jpa/HellobatchJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.study.batch.hellobatch_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellobatchJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
