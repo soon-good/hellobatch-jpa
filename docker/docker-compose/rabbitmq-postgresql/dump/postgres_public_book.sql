458713493d2cf743340e55cfd4fc4e59a6ca0b85
gosgjung <playmymind@daum.net>
2021-06-23 17:00:48 +0900

    스프링 배치 작업 스키마, 배치 테스트용도 테이블 커밋

--- docker/docker-compose/rabbitmq-postgresql/dump/postgres_public_book.sql
+++ docker/docker-compose/rabbitmq-postgresql/dump/postgres_public_book.sql
@@ -1,9 +1,10 @@
 create table IF NOT EXISTS book
 (
-    name varchar(20),
+    name varchar(100),
     id   serial not null
         constraint book_pk
-            primary key
+            primary key,
+    price integer
 );
 
 alter table book
@@ -12,3 +13,5 @@ alter table book
 INSERT INTO public.book (name, id) VALUES ('시인은 저녁에 감성이 돋는다', 1);
 INSERT INTO public.book (name, id) VALUES ('오늘은 이만 쉴께요', 2);
 INSERT INTO public.book (name, id) VALUES ('사업왕 심길후', 3);
+INSERT INTO public.book (name, id) VALUES ('빅데이터 저장 및 분석을 위한 NoSQL & Redis', 4);
+INSERT INTO public.book (name, id) VALUES ('Redis 운영 관리', 5);
