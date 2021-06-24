d1fb0d29d224add4d5b3c5945a0059f260d7c1a3
gosgjung <playmymind@daum.net>
2021-06-24 10:12:11 +0900

    단순 printJob 추가

---
+++ src/main/java/io/study/batch/hellobatch_jpa/config/batch/BookJobConfig.java
@@ -0,0 +1,92 @@
+package io.study.batch.hellobatch_jpa.config.batch;
+
+import io.study.batch.hellobatch_jpa.shop.book.Book;
+import io.study.batch.hellobatch_jpa.shop.book.BookService;
+import io.study.batch.hellobatch_jpa.shop.book.repository.BookRepository;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Configuration
+public class BookJobConfig {
+
+    @Autowired
+    private BookService bookService;
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final BookItemProcessor bookItemProcessor;
+    private final EntityManagerFactory entityManagerFactory;
+
+    public BookJobConfig(JobBuilderFactory jobBuilderFactory,
+                         StepBuilderFactory stepBuilderFactory,
+                         BookItemProcessor processor,
+                         EntityManagerFactory entityManagerFactory){
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.bookItemProcessor = processor;
+        this.entityManagerFactory = entityManagerFactory;
+    }
+
+    @Bean
+//    @JobScope
+    public Job bookPrintJob(){
+        LocalDateTime now = LocalDateTime.now();
+        String requestDate = now.format(DateTimeFormatter.ofPattern("yyyyMMddhhmmss"));
+
+        JobParameters reqDate = new JobParametersBuilder().addDate("reqDate", Date.from(now.atZone(ZoneId.systemDefault()).toInstant())).toJobParameters();
+
+        return jobBuilderFactory.get("bookPrintJob")
+                .preventRestart()
+                .start(bookPrintStep(requestDate))
+                .build();
+    }
+
+    @Bean
+//    @StepScope
+    public Step bookPrintStep(@Value("#{jobParameters[requestDate]}") String requestDate){
+        return stepBuilderFactory.get("bookPrintStepBuilder")
+                .<Book, Book>chunk(10)
+                .reader(bookSelectAllReader())
+                .processor(bookItemProcessor)
+                .writer(bookSelectAllWriter())
+                .build();
+    }
+
+    @Bean(name = "bookSelectAllReader")
+    public ListItemReader<Book> bookSelectAllReader(){
+        List<Book> list = bookRepository.findAll();
+        ListItemReader<Book> bookListReader = new ListItemReader<>(list);
+        return bookListReader;
+    }
+
+    @Bean(name = "bookSelectAllWriter")
+    public JpaItemWriter<Book> bookSelectAllWriter(){
+        JpaItemWriter<Book> jpaItemWriter = new JpaItemWriter<>();
+        jpaItemWriter.setEntityManagerFactory(entityManagerFactory);
+        return jpaItemWriter;
+    }
+
+}
